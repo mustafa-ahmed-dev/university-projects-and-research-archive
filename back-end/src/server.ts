360d6c45b944b293c0d8325b30189e2710dcbdac
Mustafa194 <61584974+Mustafa194@users.noreply.github.com>
2023-04-11 14:26:19 +0300

    First commit

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,24 +2,7 @@ import App from "./api";
 import config from "./config";
 
 import CollegeController from "./api/college/college.controller";
-import DepartmentController from "./api/department/department.controller";
-import UserController from "./api/user/user.controller";
-import PermissionController from "./api/permission/permission.controller";
-import SupervisorController from "./api/supervisor/supervisor.controller";
-import ProjectController from "./api/project/project.controller";
-import StudentController from "./api/student/student.controller";
 
-const app = new App(
-  [
-    new CollegeController(),
-    new DepartmentController(),
-    new UserController(),
-    new PermissionController(),
-    new SupervisorController(),
-    new ProjectController(),
-    new StudentController(),
-  ],
-  config.api.port
-);
+const app = new App([new CollegeController()], config.api.port);
 
 app.listen();
